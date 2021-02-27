9efc758a7e34b23a5f3b883983979fc960f48a01
Gift <gift.weike@gmail.com>
2021-02-27 22:02:56 +0100

    Initial commit

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Todo.css'
+import Header from '../Header/Header'
+const Todo = ({})=>{
+   return(
+      <div className="todo">
+         <Header/>
+      </div>
+   )
+}
+export default Todo
---
+++ src/components/UI/Card/Card.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import './Card.css'
+const Card= ({children})=><div className="Card">{children}</div>
+export default Card
